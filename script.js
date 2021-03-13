1f936898808b25cde6d92823225fd4c05938b8b7
Warastory <egastill@live.com>
2021-03-14 01:28:16 +0700

    Code Typer features completely added into main section page

--- script.js
+++ script.js
@@ -38,3 +38,34 @@ function changeIconContact() {
 		document.getElementById("icon3").className = "fa fa-caret-up";
 	}
 }
+
+function execute_typer(codetyper){
+	let resultan = codetyper.value;
+	if(resultan == "Interest"){
+		let dropdownContent = document.querySelector('#interest-info');
+		if (dropdownContent.style.display === "flex"){
+			alert("Informasi Interest dari Ega telah ditampilkan!");
+		}else{
+			dropdownContent.style.display = "flex";
+			changeIconInterest();
+		}	
+	}else if (resultan == "Skill") {
+		let dropdownContent = document.querySelector('#skill-info');
+		if (dropdownContent.style.display === "flex"){
+			alert("Informasi Skill dari Ega telah ditampilkan!");
+		}else{
+			dropdownContent.style.display = "flex";
+			changeIconSkill();
+		}
+	}else if (resultan == "Contact") {
+		let dropdownContent = document.querySelector('#contact-info');
+		if (dropdownContent.style.display === "flex"){
+			alert("Informasi Contact dari Ega telah ditampilkan!");
+		}else{
+			dropdownContent.style.display = "flex";
+			changeIconContact();
+		}
+	}else{
+		alert("Maaf perintah error atau kategori perintah belum tersedia");
+	}
+}
